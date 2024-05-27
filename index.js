4cbb4742f2b4dcac27c208876176a8e156078486
milad145 <miladaslani1991@gmail.com>
2024-05-27 16:46:44 +0330

    first-step

--- index.js
+++ index.js
@@ -5,13 +5,6 @@
  * port: Number
  * }}
  */
-import dotenv from 'dotenv';
-import config from 'config';
-
-// initiate config
-global.SecConfig = dotenv.config().parsed;
-global.EnvConfig = config.get('config');
-
 
 import {initLogs, initSecurityLog, APP_LOG} from './lib/utils/logs.js';
 
