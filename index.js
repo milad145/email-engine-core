d5aec043e9542e386610151e4b8b8824778b09b2
milad145 <miladaslani1991@gmail.com>
2024-05-29 03:32:43 +0330

    add comments

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
-/* server.js */
-
-/**
- * @type {{
- * port: Number
- * }}
- */
-
+//initiate log files
 import {initLogs, initSecurityLog, APP_LOG} from './lib/utils/logs.js';
 
 initLogs(APP_LOG);
 initSecurityLog();
 
+//initiate server
 import initServer from './lib/init-server.js';
 
 initServer();
